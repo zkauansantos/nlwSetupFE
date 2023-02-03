d116d9a9b4322fee733a62ecd137767c8f161ed4
zkauansantos <kauansantosdepontes@gmail.com>
2023-02-03 10:23:29 -0300

    created summary table

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,10 +8,10 @@ export default function Header () {
 
 			<button
 				type='button'
-				className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center justify-center gap-3 hover:border-violet-300'
+				className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300'
 			>
-				<Plus size={20} className="text-violet-500"/>
-    Novo hábito
+				<Plus size={20} className="text-violet-500 "/>
+        Novo hábito
 			</button>
 		</div>
 	);
--- src/components/SummaryTable/index.tsx
+++ src/components/SummaryTable/index.tsx
@@ -1,6 +1,24 @@
+import HabitDay from './HabitDay';
+
 export default function SummaryTable () {
+
+	const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+
 	return (
-		<>
-		</>
+		<div className="w-full flex">
+			<div className="grid grid-rows-7 grid-flow-row gap-3 border rounded-lg border-zinc-600 mr-1">
+				{weekDays.map((weekDay, index) => {
+					return (
+						<div key={`${weekDay} - ${index}`} className="text-zinc-400 text-xl font-bold h-10 w-10 flex items-center justify-center" >
+							{weekDay}
+						</div>
+					);
+				})}
+			</div>
+
+			<div className="grid grid-rows-7 grid-flow-col gap-3 " >
+				<HabitDay />
+			</div>
+		</div>
 	);
 }
